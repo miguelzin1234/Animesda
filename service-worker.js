b9656d76694f9939057271d796d8a5eb3989d5b7
miguelzin1234 <miguelhenriqueklm7@gmail.com>
2026-09-21 05:19:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,32 @@
-const CACHE_NAME="anime-tracker-v2";
-const FILES=[
- "./",
- "./index.html",
- "./manifest.json",
- "./icone.png"
+const CACHE_NAME = "anime-tracker-v1";
+const files = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icone.jpg"
 ];
 
-self.addEventListener("install",event=>{
- event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(files))
+  );
 });
 
-self.addEventListener("fetch",event=>{
- event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener("activate",event=>{
- event.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      )
+    )
+  );
 });
